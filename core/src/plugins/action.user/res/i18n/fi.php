b5a98a63835952c4c4e25eb0e2d082a859923ae5
Veikko Skurnik <veikko.skurnik@psit.fi>
2017-05-17 13:28:00 +0300

    added translations

---
+++ core/src/plugins/action.user/res/i18n/fi.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * Copyright 2007-2017 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <https://pydio.com>.
+ */
+$mess=array(
+    "title"=> "Oma tili",
+    "desc" => "Hallitse tilin asetuksia, osoitekirjaa, jaettuja elementtejä, jne.",
+    "1" => "Osoitekirja",
+    "2" => "Jaetut kansiot",
+    "3" => "Jaetut tiedostot",
+    "4"	=> "Tiedostopolku",
+    "5"	=> "Tietovarasto",
+    "6"	=> "Salasana",
+    "7"	=> "Voimassaolo",
+    "8" => "Elementit",
+    "9" => "Liittyvät käyttäjät",
+    "10"=> "Liittyvät työtilat",
+    "11"=> "Are you sure that you want to delete the selected element(s)?",
+    "12"=> "You don't have the permission to delete this element.",
+    "13"=> "Successfully deleted public file.",
+    "14"=> "Integrity",
+    "15"=> "Ok",
+    "16"=> "Rikki",
+    "17"=> "Lataus URL",
+    "18"=> "Kopioi URL",
+    "19"=> "Kopioi url sähköpostilähetystä varten.",
+    "20"=> "Lataukset",
+    "21"=> "Kyllä",
+    "22"=> "Ei",
+    "23"=> "Successfully deleted %s files.",
+    "24"=> "Nothing to delete.",
+    "25"=> "Clear Expired",
+    "26"=> "Delete expired shared files.",
+    "27"=> "Omistaja",
+    "28"=> "Files shared for direct download, either protected by a password or not.",
+    "29"=> "Delegates workspaces created by users.",
+    "30"=> "Users created to access the delegated workspaces.",
+    "31"=> "Description",
+    "32"=> "Reset",
+    "33"=> "Reset download counter",
+    "34"=> "Omat jaot",
+    "35"=> "All my shared items (files and folders)",
+    "36"=> "Parameters",
+    "37"=> "My profile and application settings",
+    "38"=> "Notifications",
+    "39"=> "Below is a list of the workspaces you can access to. They can be personal or shared with/by others users.",
+    "40"=> "Welcome, %s! ",
+    "41"=> "Use this workspace as default",
+    "42"=> "Enter",
+    "43"=> "Profiilini",
+    "43t"=> "All my personal data",
+    "44"=> "Oma tili",
+    "45"=> "Omat tiedot",
+    "46"=> "Osoitekirja",
+    "47"=> "Users and teams that you created",
+    "48"=> "Omat käyttäjät",
+    "49"=> "External users that you created",
+    "50"=> "Omat tiimit",
+    "51"=> "Teams of users, used as shortcuts when sharing",
+    "52"=> "Are you sure you want to delete this team? This will not remove any users.",
+    "53"=> "Back to my workspaces",
+    "54"=> "Update Members",
+    "55"=> "Add/Remove users from this team",
+    "56"=> "Add one user per line, type in the textarea to see the auto-completion list appear.",
+    "57" => "Create a Team",
+    "58" => "Teams are used as a shortcut to quickly share a folder with a predefined set of users",
+    "59" => "Team Label",
+    "60" => "Team Users",
+    "61" => "Email Preferences",
+    "62" => "Send emails every X minutes",
+    "63" => "Send emails every Y hours",
+    "64" => "Choose the hour of the day",
+    "65" => "Choose the first hour of the day",
+    "66" => "Choose the second hour of the day",
+    "67" => "Pick a day of the week",
+    "68" => "Maanantai",
+    "69" => "Tiistai",
+    "70" => "Keskiviikko",
+    "71" => "Torstai",
+    "72" => "Perjantai",
+    "73" => "Lauantai",
+    "74" => "Sunnuntai",
+);
